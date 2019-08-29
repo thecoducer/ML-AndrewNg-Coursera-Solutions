52add0e38c44eedf79d27a940f7644322853a649
thecoducer <mayukh5741@gmail.com>
2019-08-29 20:56:08 +0530

    update

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -7,8 +7,8 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 
 % You need to set these values correctly
 X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
+mu = zeros(1, size(X, 2)); % 2 means column here
+sigma = zeros(1, size(X, 2)); % 2 means column here
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
@@ -26,13 +26,21 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
+% mu = mean(X);
+% sigma = std(X);
+% 
+% X_norm = X_norm .- mu;
+% X_norm = X_norm ./ sigma;
+% 
+% X = X_norm;
 
+mu = mean(X);
+sigma = std(X);
 
+X_norm = X_norm - mu;
+X_norm = X_norm ./ sigma;
 
-
-
-
-
+X = X_norm;
 
 % ============================================================
 
